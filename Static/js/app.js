29c7cffcb807dc2bb661aab93a75943dc83f30fc
GJS2002 <67513182+GJS2002@users.noreply.github.com>
2020-07-04 18:03:34 -0500

    Responsive and working

--- Static/js/app.js
+++ Static/js/app.js
@@ -3,14 +3,30 @@ const btnClose = document.querySelector(".btn-close")
 const modal = document.querySelector(".modal");
 const images = document.querySelectorAll("img");
 const body = document.querySelector("body");
-let src;
+
 
 btnBackground.addEventListener('click', () => {
-     document.documentElement.style.setProperty('--height', '100vh');
+     console.log(window.innerHeight);
+     if(window.innerHeight === 1366){
+          document.documentElement.style.setProperty('--width', '50vw');
+     } else if(window.innerWidth > 1000){
+          document.documentElement.style.setProperty('--width', '30vw');
+     }
+      else {
+          document.documentElement.style.setProperty('--height', '100vh');
+     }
+     
 });
 
 btnClose.addEventListener('click', () => {
-     document.documentElement.style.setProperty('--height', '0vh');
+     if(window.innerWidth > 1000){
+          document.documentElement.style.setProperty('--width', '0vw');
+          console.log('yeah');
+          
+     } else {
+          document.documentElement.style.setProperty('--height', '0vh');
+     }
+     
 });
 
 images.forEach(img => {
@@ -18,8 +34,8 @@ images.forEach(img => {
           let img = e.currentTarget;
           images.forEach(img => img.classList.remove("active"));
           img.classList.add("active");
-          src = img.src;
-          body.style.backgroundImage = `url(${src})`;
+          let src = img.src;
+          body.style.background = `center / cover no-repeat url(${src})`;
      });
 });
 
