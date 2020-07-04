cbcd72ef39df37fb11538b96b7a5a07f0b5a5d43
GJS2002 <67513182+GJS2002@users.noreply.github.com>
2020-07-04 14:55:21 -0500

    Added functionality and mobile side view responsive

--- Static/js/app.js
+++ Static/js/app.js
@@ -1,6 +1,9 @@
 const btnBackground = document.querySelector(".btn-background");
 const btnClose = document.querySelector(".btn-close")
 const modal = document.querySelector(".modal");
+const images = document.querySelectorAll("img");
+const body = document.querySelector("body");
+let src;
 
 btnBackground.addEventListener('click', () => {
      document.documentElement.style.setProperty('--height', '100vh');
@@ -9,3 +12,14 @@ btnBackground.addEventListener('click', () => {
 btnClose.addEventListener('click', () => {
      document.documentElement.style.setProperty('--height', '0vh');
 });
+
+images.forEach(img => {
+     img.addEventListener('click', e => {
+          let img = e.currentTarget;
+          images.forEach(img => img.classList.remove("active"));
+          img.classList.add("active");
+          src = img.src;
+          body.style.backgroundImage = `url(${src})`;
+     });
+});
+
